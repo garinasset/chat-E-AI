195676637029d54faa5a8bde1778ee362336a45b
JiaLin <comu@qq.com>
2023-12-17 04:09:17 +0800

    1.0.4

--- AIs/OpenAI/AI_OpenAI.py
+++ AIs/OpenAI/AI_OpenAI.py
@@ -97,7 +97,7 @@ class AIOpenAI(AI):
                 """记录日志"""
                 loggerOpenAI.error(e.message)
 
-                _response.answer = e.body
+                _response.answer = e.message
                 return _response
             except openai.APIStatusError as e:
                 """记录日志"""
